2602f4c2246efb43e9ca79d2d1af54e10ecabbe1
fmz620 <mzfahri620@gmail.com>
2018-01-20 22:13:52 +0700

    beberapa kolom di db_ujian nya ada yang diubah

---
+++ db_ujian.sql
@@ -0,0 +1,179 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 10.1.26-MariaDB : Database - db_ujian
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`db_ujian` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `db_ujian`;
+
+/*Table structure for table `guru` */
+
+DROP TABLE IF EXISTS `guru`;
+
+CREATE TABLE `guru` (
+  `nip` varchar(50) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `bidang_keahlian` text,
+  `nama` varchar(150) NOT NULL,
+  `alamat` text,
+  `jenis_kelamin` char(1) DEFAULT NULL,
+  `email` varchar(50) NOT NULL,
+  `foto` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`nip`),
+  KEY `id_user` (`id_user`),
+  CONSTRAINT `guru_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `guru` */
+
+insert  into `guru`(`nip`,`id_user`,`bidang_keahlian`,`nama`,`alamat`,`jenis_kelamin`,`email`,`foto`) values ('1502011309',1,'Apa aja','tester','Jalan','L','test@test.com','nope.jpg');
+
+/*Table structure for table `kelas` */
+
+DROP TABLE IF EXISTS `kelas`;
+
+CREATE TABLE `kelas` (
+  `id_kelas` int(11) NOT NULL AUTO_INCREMENT,
+  `nama_kelas` varchar(20) NOT NULL,
+  PRIMARY KEY (`id_kelas`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*Data for the table `kelas` */
+
+insert  into `kelas`(`id_kelas`,`nama_kelas`) values (1,'XII RPL 2');
+
+/*Table structure for table `mapel` */
+
+DROP TABLE IF EXISTS `mapel`;
+
+CREATE TABLE `mapel` (
+  `id_mapel` int(11) NOT NULL AUTO_INCREMENT,
+  `nama_mapel` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id_mapel`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*Data for the table `mapel` */
+
+insert  into `mapel`(`id_mapel`,`nama_mapel`) values (1,'webdin');
+
+/*Table structure for table `nilai` */
+
+DROP TABLE IF EXISTS `nilai`;
+
+CREATE TABLE `nilai` (
+  `id_nilai` int(11) NOT NULL AUTO_INCREMENT,
+  `id_ujian` int(11) NOT NULL,
+  `nis` varchar(10) NOT NULL,
+  `jawaban_benar` int(11) NOT NULL,
+  `jawaban_salah` int(11) NOT NULL,
+  `nilai` int(11) NOT NULL,
+  PRIMARY KEY (`id_nilai`),
+  KEY `id_ujian` (`id_ujian`),
+  KEY `nis` (`nis`),
+  CONSTRAINT `nilai_ibfk_1` FOREIGN KEY (`id_ujian`) REFERENCES `ujian` (`id_ujian`),
+  CONSTRAINT `nilai_ibfk_2` FOREIGN KEY (`nis`) REFERENCES `siswa` (`nis`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*Data for the table `nilai` */
+
+insert  into `nilai`(`id_nilai`,`id_ujian`,`nis`,`jawaban_benar`,`jawaban_salah`,`nilai`) values (1,1,'1502011309',1,1,50);
+
+/*Table structure for table `siswa` */
+
+DROP TABLE IF EXISTS `siswa`;
+
+CREATE TABLE `siswa` (
+  `nis` varchar(10) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_kelas` int(11) NOT NULL,
+  `nama` varchar(150) NOT NULL,
+  `alamat` text,
+  `jenis_kelamin` char(1) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `jurusan` varchar(50) NOT NULL,
+  `foto` varchar(50) NOT NULL,
+  PRIMARY KEY (`nis`),
+  KEY `id_user` (`id_user`),
+  KEY `id_kelas` (`id_kelas`),
+  CONSTRAINT `siswa_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`),
+  CONSTRAINT `siswa_ibfk_2` FOREIGN KEY (`id_kelas`) REFERENCES `kelas` (`id_kelas`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `siswa` */
+
+insert  into `siswa`(`nis`,`id_user`,`id_kelas`,`nama`,`alamat`,`jenis_kelamin`,`email`,`jurusan`,`foto`) values ('1502011309',2,1,'Fahri','Cigondewah','L','mzfahri620@gmail.com','RPL','nopenope.png');
+
+/*Table structure for table `soal` */
+
+DROP TABLE IF EXISTS `soal`;
+
+CREATE TABLE `soal` (
+  `id_soal` int(11) NOT NULL AUTO_INCREMENT,
+  `id_ujian` int(11) NOT NULL,
+  `tipe` varchar(50) NOT NULL,
+  `isi_soal` text NOT NULL,
+  `pilihan` text NOT NULL,
+  `jawaban` text NOT NULL,
+  PRIMARY KEY (`id_soal`),
+  KEY `id_ujian` (`id_ujian`),
+  CONSTRAINT `soal_ibfk_1` FOREIGN KEY (`id_ujian`) REFERENCES `ujian` (`id_ujian`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Data for the table `soal` */
+
+insert  into `soal`(`id_soal`,`id_ujian`,`tipe`,`isi_soal`,`pilihan`,`jawaban`) values (1,1,'BS','Ini Benar.','Benar;Salah','Benar'),(2,1,'BS','Ini Salah.','Benar;Salah','Benar');
+
+/*Table structure for table `ujian` */
+
+DROP TABLE IF EXISTS `ujian`;
+
+CREATE TABLE `ujian` (
+  `id_ujian` int(11) NOT NULL AUTO_INCREMENT,
+  `id_mapel` int(11) NOT NULL,
+  `nip` varchar(50) NOT NULL,
+  `judul_ujian` varchar(50) DEFAULT NULL,
+  `waktu_pengerjaan` time NOT NULL,
+  `tanggal_post` date NOT NULL,
+  `tanggal_kadaluarsa` date NOT NULL,
+  `status` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_ujian`),
+  KEY `id_mapel` (`id_mapel`),
+  KEY `nip` (`nip`),
+  CONSTRAINT `ujian_ibfk_1` FOREIGN KEY (`id_mapel`) REFERENCES `mapel` (`id_mapel`),
+  CONSTRAINT `ujian_ibfk_2` FOREIGN KEY (`nip`) REFERENCES `guru` (`nip`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*Data for the table `ujian` */
+
+insert  into `ujian`(`id_ujian`,`id_mapel`,`nip`,`judul_ujian`,`waktu_pengerjaan`,`tanggal_post`,`tanggal_kadaluarsa`,`status`) values (1,1,'1502011309','Dasar dasar PHP','00:20:00','2018-01-20','2018-01-27','0');
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(25) NOT NULL,
+  `password` varchar(150) NOT NULL,
+  `hak_akses` varchar(15) NOT NULL,
+  PRIMARY KEY (`id_user`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Data for the table `user` */
+
+insert  into `user`(`id_user`,`username`,`password`,`hak_akses`) values (1,'Tester','123','admin'),(2,'Siswa_tester','123456','siswa');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
