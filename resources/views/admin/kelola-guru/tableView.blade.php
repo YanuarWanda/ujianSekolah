d20e9c33572050b4133ffcd7b0a6b4acec33f427
Fahri Muhamad <mzfahri620@gmail.com>
2018-02-24 21:50:27 +0700

    Fix tableView & detail guru

--- resources/views/admin/kelola-guru/tableView.blade.php
+++ resources/views/admin/kelola-guru/tableView.blade.php
@@ -16,7 +16,6 @@
                                     <th>No.</th>
                                     <th>NIP</th>
                                     <th>Nama</th>
-                                    <th>Bidang Keahlian</th>
                                     <th>Aksi</th>
                                 </tr>
                             </thead>
@@ -28,11 +27,10 @@
                                             <td><?php echo $no;$no++; ?></td>
                                             <td>{{$g->nip}}</td>
                                             <td>{{$g->nama}}</td>
-                                            <td>{{$g->bidang_keahlian}}</td>
                                             <td>
-                                                <a href="{{url('/kelola-guru/show', base64_encode($g->nip))}}" class="btn btn-warning"><i class="fa fa-eye" aria-hidden="true"></i></a>
-                                                <a href="{{url('/kelola-guru/edit', base64_encode($g->nip))}}" class="btn btn-primary"><i class="fa fa-edit" aria-hidden="true"></i></a>
-                                                <a href="{{url('/kelola-guru/delete', base64_encode($g->nip))}}" class="btn btn-danger"><i class="fa fa-trash" aria-hidden="true"></i></a>
+                                                <a href="{{url('/kelola-guru/show', base64_encode($g->id_guru))}}" class="btn btn-warning"><i class="fa fa-eye" aria-hidden="true"></i></a>
+                                                <a href="{{url('/kelola-guru/edit', base64_encode($g->id_guru))}}" class="btn btn-primary"><i class="fa fa-edit" aria-hidden="true"></i></a>
+                                                <a href="{{url('/kelola-guru/delete', base64_encode($g->id_guru))}}" class="btn btn-danger"><i class="fa fa-trash" aria-hidden="true"></i></a>
                                             </td>
                                         </tr>
                                     @endforeach
