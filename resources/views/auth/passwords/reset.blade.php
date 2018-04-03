eb035e58e8a80064b7792f2d97d6df89f0bf2aa2
Fahri Muhamad <mzfahri620@gmail.com>
2018-04-03 16:56:33 +0700

    Show Hide Password

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 
+@section('css')
+<link rel="stylesheet" type="text/css" href="{{ asset('css/login.css') }}">
+@stop
+
 @section('content')
 <div class="container">
     <div class="row">
--- resources/views/custom-auth/login.blade.php
+++ resources/views/custom-auth/login.blade.php
@@ -6,49 +6,62 @@
 
 @section('content')
 <div class="limiter">
-        <div class="container-login100">
-            <div class="wrap-login100 p-t-50 p-b-90">
-                <form class="form-horizontal login100-form flex-sb flex-w" method="POST" action="{{ route('login') }}">
-                    {{ csrf_field() }}
-                    <span class="login100-form-title p-b-51">
+    <div class="container-login100">
+        <div class="wrap-login100 p-t-50 p-b-90">
+            <form class="form-horizontal login100-form flex-sb flex-w" method="POST" action="{{ route('login') }}">
+                {{ csrf_field() }}
+                <span class="login100-form-title p-b-51">
+                    Login
+                </span>
+
+                
+                <div class="wrap-input100 validate-input m-b-16" data-validate = "Username is required">
+                    <input class="input100" type="text" name="username" placeholder="Username">
+                    <span class="focus-input100"></span>
+                </div>
+                
+                
+                <div class="input-group wrap-input100 validate-input m-b-16" data-validate = "Password is required">
+                    <input class="input100" type="password" id="password" name="password" placeholder="Password">
+                    <span class="focus-input100"></span>
+                </div>
+                
+                <div class="flex-sb-m w-full p-t-3 p-b-24">
+                    <div class="contact100-form-checkbox">
+                        <input class="input-checkbox100" id="show-password" type="checkbox">
+                        <label class="label-checkbox100" for="show-password">
+                            Show Password
+                        </label>
+                    </div>
+
+                    <div>
+                        <a href="{{ route('password.request') }}" class="txt1">
+                            Forgot Password?
+                        </a>
+                    </div>
+                </div>
+
+                <div class="container-login100-form-btn m-t-17">
+                    <button class="login100-form-btn" type="submit">
                         Login
-                    </span>
+                    </button>
+                </div>
 
-                    
-                    <div class="wrap-input100 validate-input m-b-16" data-validate = "Username is required">
-                        <input class="input100" type="text" name="username" placeholder="Username">
-                        <span class="focus-input100"></span>
-                    </div>
-                    
-                    
-                    <div class="wrap-input100 validate-input m-b-16" data-validate = "Password is required">
-                        <input class="input100" type="password" name="password" placeholder="Password">
-                        <span class="focus-input100"></span>
-                    </div>
-                    
-                    <div class="flex-sb-m w-full p-t-3 p-b-24">
-                        <div class="contact100-form-checkbox">
-                            <input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
-                            <label class="label-checkbox100" for="ckb1">
-                                Remember me
-                            </label>
-                        </div>
-
-                        <div>
-                            <a href="#" class="txt1">
-                                Forgot?
-                            </a>
-                        </div>
-                    </div>
-
-                    <div class="container-login100-form-btn m-t-17">
-                        <button class="login100-form-btn" type="submit">
-                            Login
-                        </button>
-                    </div>
-
-                </form>
-            </div>
+            </form>
         </div>
     </div>
+</div>
 @endsection
+
+@section('js')
+<script type="text/javascript">
+    $('#show-password').click(function(e) {
+        var password = $('#password');
+        if($(this). prop("checked") == true){
+            password.attr( 'type', 'text' );
+        }else {
+            password.attr( 'type', 'password' );
+        }
+    });
+</script>
+@stop
