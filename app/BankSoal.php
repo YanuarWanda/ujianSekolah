a99b2e243663b4ccc74dbf02079f29edbd8f51e8
Fahri Muhamad <mzfahri620@gmail.com>
2018-04-09 23:42:19 +0700

    apasih ini

--- app/BankSoal.php
+++ app/BankSoal.php
@@ -18,4 +18,8 @@ class BankSoal extends Model
     public function daftarBidangKeahlian() {
     	return $this->belongsTo('App\DaftarBidangKeahlian', 'id_daftar_bidang');
     }
+
+    public function soal() {
+    	return $this->hasMany('App\Soal', 'id_bank_soal');
+    }
 }
